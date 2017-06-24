4a91254285fee135da82b94fc46abf6148a92782
1661sons <julia.kerouac@gmail.com>
2017-06-24 19:06:43 -0400

    cleaned up spacing in js file and rewrote comments

--- js/marked.js
+++ js/marked.js
@@ -1,21 +1,17 @@
 jQuery(document).ready(function() {
     jQuery('button').click(function() {
-        //make sure the click is being heard
+        //make sure click event is being heard
         console.log('clicked2!')
 
-        //add or remove bat signal image on click
-        //jQuery(this).toggleClass('marked');
-
-        //if has class 'marked', toggle button text
+        //if has class 'marked', turn on green button background and change text
         if (!jQuery(this).hasClass('marked')) {
             jQuery(this).addClass('marked');
             jQuery(this).empty();
             jQuery(this).append('Read It!<i class="material-icons left">bookmark</i>');
         }
-        // else (jQuery('button').text('Mark Read'));
+        //class does not have class 'marked', turn off green button background and change text
         else {
             jQuery(this).removeClass('marked');
-
             jQuery(this).empty();
             jQuery(this).append('mark read<i class="material-icons left">bookmark_outline</i>');
         }
